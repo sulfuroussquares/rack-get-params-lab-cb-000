e80476d0c8cceb85010249b7150cf8d0ba8cf92c
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-11-24 17:14:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,8 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add)
+      #dostuff
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty."
